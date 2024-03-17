0205a398dbef34c4d25d3a47abf4136e7538e04e
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-03-17 21:37:39 +0000

    wip

--- supabasedata.js
+++ supabasedata.js
@@ -1,17 +1,14 @@
-<script>
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
-const supabaseUrl = 'https://hcqebjnptjgxiilhbajh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcWViam5wdGpneGlpbGhiYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyNzAxNzUsImV4cCI6MjAyNTg0NjE3NX0.a-bx7aMAkGA2QjRPa3zoObjV5EoQmnYZraMHlGFIN38'
-const supabase = createClient(supabaseUrl, supabaseKey)
+  import { createClient } from "https://esm.sh/@supabase/supabase-js";
+  const supabaseUrl = 'https://hcqebjnptjgxiilhbajh.supabase.co';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcWViam5wdGpneGlpbGhiYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyNzAxNzUsImV4cCI6MjAyNTg0NjE3NX0.a-bx7aMAkGA2QjRPa3zoObjV5EoQmnYZraMHlGFIN38';
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function getBooks()
-{
-  
-let { data: Books, error } = await supabase
-  .from('Books')
-  .select('*')
+  async function getBooks() {
+    let { data: Books, error } = await supabase
+      .from('Books')
+      .select('*');
 
- let tableBody = document.getElementById('BooksBody');
+    let tableBody = document.getElementById('BooksBody');
     for (let book of Books) {
       let row = document.createElement('tr');
       row.innerHTML = `
@@ -22,5 +19,5 @@ let { data: Books, error } = await supabase
       tableBody.appendChild(row);
     }
   }
-getBooks();
-</script>
+
+  getBooks();
