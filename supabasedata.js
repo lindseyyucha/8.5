6cfd66dfba32ee8faa414fad253e843ae31d2b2e
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-03-17 20:37:18 +0000

    wip

--- supabasedata.js
+++ supabasedata.js
@@ -1,3 +1,4 @@
+<script>
 import { createClient } from "https://esm.sh/@supabase/supabase-js"
 const supabaseUrl = 'https://hcqebjnptjgxiilhbajh.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcWViam5wdGpneGlpbGhiYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyNzAxNzUsImV4cCI6MjAyNTg0NjE3NX0.a-bx7aMAkGA2QjRPa3zoObjV5EoQmnYZraMHlGFIN38'
@@ -22,3 +23,4 @@ let { data: Books, error } = await supabase
     }
   }
 getBooks();
+</script>
